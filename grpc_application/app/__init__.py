fd9dc16242058f2c5cf143650556d78669674a82
asdf <migsking@naver.com>
2020-08-20 17:39:54 +0900

    한번더  추상화

--- grpc_application/app/__init__.py
+++ grpc_application/app/__init__.py
@@ -1,19 +1,17 @@
 import grpc
 from concurrent import futures
 
-from grpc_application.proto.example1 import example1_pb2, example1_pb2_grpc
-from grpc_application.proto.example2 import example2_pb2, example2_pb2_grpc
 from grpc_application.config.app_config import DevLevelConfig
+from grpc_application.app.servicers.example import register_example_servicers
+
 
 def register_servicers(app):
-    example1_pb2_grpc.add_Example1Servicer_to_server(Example1(), app)
-    example2_pb2_grpc.add_Example2Servicer_to_server(Example2(), app)
+    register_example_servicers(app)
 
 
 def create_app():
     app = grpc.server(futures.ThreadPoolExecutor(max_workers=DevLevelConfig.worker_node))
+    app.add_insecure_port("localhost:50051")
 
     register_servicers(app)
-
-    app.add_insecure_port("localhost:50051")
     return app
