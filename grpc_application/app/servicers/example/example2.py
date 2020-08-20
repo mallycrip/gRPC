049078648196508d55da35e959aa419a918ed58e
asdf <migsking@naver.com>
2020-08-20 19:55:57 +0900

    예제 변경

--- grpc_application/app/servicers/example/example2.py
+++ grpc_application/app/servicers/example/example2.py
@@ -1,5 +1,11 @@
+from grpc_application.proto.example2 import example2_pb2, example2_pb2_grpc
+from grpc_application.app.services.example.example1 import Example1Service
+
+
 class Example2(example2_pb2_grpc.Example2Servicer):
     def CreateData2(self, request, context):
-        print("CreatedData1")
-        if request.data is None: return example2_pb2.CreateData2Response(message="No Context", code=400)
-        return example2_pb2.CreateData2Response(message="Created in 2", code=201)
+        return Example1Service.create_data(request)
+
+
+    def ReadData2(self, request, context):
+        return Example1Service.read_data(request)
